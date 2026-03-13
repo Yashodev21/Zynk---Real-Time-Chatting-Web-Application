63afe6a744e49d2f145cb6b39a07420ef789f006
Yashodev Rout <yash21054@gmail.com>
2026-03-13 19:36:33 +0530

    corrected files

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ import { app, server } from "./lib/socket.js";
 
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 /* Middlewares */
 app.use(express.json({ limit: "5mb" }));
